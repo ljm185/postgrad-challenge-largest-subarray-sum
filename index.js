672a34e1ca4a1aa2b586514540c7223bb5765e1b
ljm185 <ljmagoncia@yahoo.com>
2020-04-27 23:32:50 -0400

    done

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 function largestSubarraySum(array) {
-    let oldSum, newSum, maxSum = 0;
+    let sum = 0;
+    let maxSum = 0;
     for (i = 0; i < array.length; i++) {
         for (j = i; j < array.length; j++) {
-            newSum = oldSum + array[j];
-            if (newSum > maxSum) {
-                maxSum = newSum;
+            sum += array[j];
+            if (sum > maxSum) {
+                maxSum = sum;
             }
-            oldSum = newSum;
             console.log(maxSum);
         }
-        oldSum = array[i];
+        sum = 0;
+    }
+    return maxSum;
+}
+
+function largestSubarraySumFast(array) {
+    let sum = 0; 
+    let maxSum = 0;
+    for (i = 0; i < array.length; i++) {
+        if (sum + array[i] < 0) {
+            sum = 0;
+            console.log(maxSum);
+        } else {
+            sum += array[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            console.log(maxSum);
+        }
     }
     return maxSum;
 }
 
 let array1 = [1, 2, 3], array2 = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4];
 
-largestSubarraySum(array2);
+largestSubarraySumFast(array2);
